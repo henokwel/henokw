45d878c759e6333ee580cc8a2ee8170374fb8401
henWel <henokwel@outlook.com>
2020-09-24 00:00:32 +0200

    Updates

---
+++ precache-manifest.5fc0a175f133ebd812bda2ab61db1bb5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "119982f57669562069fdcbac7f798460",
+    "url": "/henokwel/henokw.git/index.html"
+  },
+  {
+    "revision": "9bea0641756a99067364",
+    "url": "/henokwel/henokw.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c366e3b528d77c5df781",
+    "url": "/henokwel/henokw.git/static/js/2.bd5ec5b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/henokwel/henokw.git/static/js/2.bd5ec5b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bea0641756a99067364",
+    "url": "/henokwel/henokw.git/static/js/main.926c2937.chunk.js"
+  },
+  {
+    "revision": "312a240cdaebdffb1fa3",
+    "url": "/henokwel/henokw.git/static/js/runtime-main.ab53c1ee.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/henokwel/henokw.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
